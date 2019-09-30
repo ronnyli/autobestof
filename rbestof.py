0584823e3d5674191c3df9593476d16428c67c32
Ronny <ronnyli91@gmail.com>
2019-09-30 12:13:53 -0400

    rbestof data only contains target comment

--- rbestof.py
+++ rbestof.py
@@ -7,6 +7,66 @@ api = PushshiftAPI()
 
 BEFORE_CREATED_UTC = None
 
+def parse_bestof(bestof_submission):
+    out = {}
+    out['bestof_created_utc'] = bestof_submission.created_utc
+    url_ = bestof_submission.url.split('?')
+    url = url_[0].split('/')
+    url_params = None
+    if len(url_) > 1:
+        url_params = url_[1]
+    try:
+        submission_id = url[6]
+        comment_id = None
+        if len(url) >= 9:
+            comment_id = url[8]
+        target_id = comment_id if comment_id else submission_id
+        target = target_id + ' ' + bestof_submission.title
+        out['target'] = target
+    except:
+        return
+    # generate input data
+    subtext = ['submission']
+    submission = reddit.submission(submission_id)
+    try:
+        if submission.author:
+            subtext.append('u/' + submission.author.name)
+        else:
+            subtext.append('[deleted]')
+    except:
+        return
+    out['subreddit'] = submission.subreddit.display_name
+    subtext.append(submission.title)
+    if submission.is_self:
+        subtext.append(submission.selftext)
+    else:
+        subtext.append(submission.url)
+    subtext.append(submission_id)
+    comments = submission.comments.list()
+    for comment in comments:
+        # the other alternative is MoreComments
+        if comment.__class__.__name__ == 'Comment':
+            # ignore comments after r/bestof submission
+            too_late = comment.created_utc >= bestof_submission.created_utc
+            # comments with score < 3 are definitely not r/bestof material
+            low_score = comment.score < 3
+            if low_score or too_late: continue
+            if comment.created_utc < bestof_submission.created_utc:
+                parent_id = comment.parent_id.split('_')[1]
+                if parent_id == submission_id:
+                    subtext.append('top-level')
+                else:
+                    subtext.append('reply')
+                subtext.append(parent_id)
+                if comment.author:
+                    subtext.append('u/' + comment.author.name)
+                else:
+                    subtext.append('[deleted]')
+                subtext.append(comment.body)
+                subtext.append(comment.id)
+    out['input'] = '\n'.join(subtext)
+    return out
+
 if __name__== '__main__':
     params = {
         'subreddit': 'bestof',
@@ -18,69 +78,13 @@ if __name__== '__main__':
         params['before'] = BEFORE_CREATED_UTC
     n = 0
     for bestof_submission in api.search_submissions(**params):
-        out = {}
+        n+=1
         if bestof_submission.score < 3:
             # There's a surprising amount of garbage on r/bestof
             continue
-        n+=1
-        out['bestof_created_utc'] = bestof_submission.created_utc
         if bestof_submission.subreddit != 'bestof':
             break
         if n == 10000:
             break
-        url_ = bestof_submission.url.split('?')
-        url = url_[0].split('/')
-        url_params = None
-        if len(url_) > 1:
-            url_params = url_[1]
-        try:
-            submission_id = url[6]
-            comment_id = None
-            if len(url) >= 9:
-                comment_id = url[8]
-            target_id = comment_id if comment_id else submission_id
-            target = target_id + ' ' + bestof_submission.title
-            out['target'] = target
-        except:
-            continue
-        # generate input data
-        subtext = ['submission']
-        submission = reddit.submission(submission_id)
-        try:
-            if submission.author:
-                subtext.append('u/' + submission.author.name)
-            else:
-                subtext.append('[deleted]')
-        except:
-            continue
-        out['subreddit'] = submission.subreddit.display_name
-        subtext.append(submission.title)
-        if submission.is_self:
-            subtext.append(submission.selftext)
-        else:
-            subtext.append(submission.url)
-        subtext.append(submission_id)
-        comments = submission.comments.list()
-        for comment in comments:
-            # the other alternative is MoreComments
-            if comment.__class__.__name__ == 'Comment':
-                # ignore comments after r/bestof submission
-                too_late = comment.created_utc >= bestof_submission.created_utc
-                # comments with score < 3 are definitely not r/bestof material
-                low_score = comment.score < 3
-                if low_score or too_late: continue
-                if comment.created_utc < bestof_submission.created_utc:
-                    parent_id = comment.parent_id.split('_')[1]
-                    if parent_id == submission_id:
-                        subtext.append('top-level')
-                    else:
-                        subtext.append('reply')
-                    subtext.append(parent_id)
-                    if comment.author:
-                        subtext.append('u/' + comment.author.name)
-                    else:
-                        subtext.append('[deleted]')
-                    subtext.append(comment.body)
-                    subtext.append(comment.id)
-        out['input'] = '\n'.join(subtext)
-        print(json.dumps(out))
+        out = parse_bestof(bestof_submission)
+        if out: print(json.dumps(out))
--- rbestof_json_restructure.py
+++ rbestof_json_restructure.py
@@ -1,11 +1,28 @@
+from gpt_2_simple.src import encoder
+
 import json
+import re
 import sys
+
+def find_comment(target_id, txt):
+    before_id = txt.split(target_id)[0]
+    after_username = re.split('(u/\w+?)\\n', before_id)[-2:]
+    return '\n'.join(after_username)
+
+def restrict_length(txt, n_tokens=900):
+    enc = encoder.get_encoder('checkpoint/run1/')
+    return enc.decode(enc.encode(txt)[:n_tokens])
 
 for line in sys.stdin:
     # dict_keys(['bestof_created_utc', 'input', 'target'])
     rbestof = json.loads(line)
+    target_id = rbestof['target'].split()[0]
+    comment_txt = find_comment(target_id, rbestof['input'])
+    comment_txt = restrict_length(comment_txt)
+    if len(comment_txt) == 0 or len(rbestof['target']) == 0: continue
     print('<|startoftext|>')
-    print(rbestof['input'])
+    print(target_id)
+    print(comment_txt)
     print('<|rbestof|>')
     print(rbestof['target'])
     print('<|endoftext|>')
