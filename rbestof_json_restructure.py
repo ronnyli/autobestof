66db0acf99b4751d7454ae1717ad843808fac7f6
Ronny <ronnyli91@gmail.com>
2019-10-01 21:10:53 -0400

    encode decode

--- rbestof_json_restructure.py
+++ rbestof_json_restructure.py
@@ -4,24 +4,38 @@ import json
 import re
 import sys
 
+gpt2 = Gpt2Generator()
+gpt2.get_encoder()
+
 def find_comment(target_id, txt):
     before_id = txt.split(target_id)[0]
     after_username = re.split('(u/\w+?)\\n', before_id)[-2:]
     return '\n'.join(after_username)
 
-def restrict_length(txt, n_tokens=900):
-    enc = encoder.get_encoder('checkpoint/run1/')
-    return enc.decode(enc.encode(txt)[:n_tokens])
+def restrict_length(all_txt, max_tokens=1023, output_len=30):
+    '''Max length of document must be `max_tokens` long after the encoding step'''
+    enc = gpt2.encoder
+    input_txt = all_txt.pop(1)
+    output_txt = all_txt.pop(2)
+    output_tokens = enc.encode(output_txt)[:output_len]
+    n_output = len(output_tokens)
+    output_trunc = enc.decode(output_tokens)
+    misc_txt = '\n'.join(all_txt)
+    n_misc = len(enc.encode(misc_txt))
+    n_input = max_tokens - n_output - n_misc
+    assert n_input > 960, output_trunc + ' ' + misc_txt
+    input_trunc = enc.decode(enc.encode(input_txt)[:n_input])
+    all_txt.insert(output_trunc, 2)
+    all_txt.insert(input_trunc, 1)
+    return all_txt
 
 for line in sys.stdin:
     # dict_keys(['bestof_created_utc', 'input', 'target'])
     rbestof = json.loads(line)
     target_id, target = rbestof['target'].split(maxsplit=1)
     comment_txt = find_comment(target_id, rbestof['input'])
-    comment_txt = restrict_length(comment_txt)
-    if len(comment_txt) == 0 or len(rbestof['target']) == 0: continue
-    print('<|startoftext|>')
-    print(comment_txt)
-    print('<|rbestof|>')
-    print(target)
-    print('<|endoftext|>')
+    if len(comment_txt) == 0 or len(target) == 0: continue
+    all_txt = ['<|startoftext|>', comment_txt, '<|rbestof|>', target, '<|endoftext|>']
+    all_trunc = restrict_length(all_txt)
+    for txt in all_trunc:
+        print(txt)
